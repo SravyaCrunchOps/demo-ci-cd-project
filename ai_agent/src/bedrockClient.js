65f90518c8fd44dd75219b2c5c3c209db0b9a46f
SravyaCrunchOps <sravya@crunchops.com>
2025-12-16 09:20:11 +0530

    changed bedrock config

--- ai_agent/src/bedrockClient.js
+++ ai_agent/src/bedrockClient.js
@@ -1,4 +1,4 @@
-import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime'
+import { BedrockRuntimeClient, ConverseCommand } from '@aws-sdk/client-bedrock-runtime'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -14,19 +14,25 @@ const client = new BedrockRuntimeClient({
 
 export async function invokeBedrockLLM(intent) {
     const model = process.env.BEDROCK_MODEL_ID || 'anthropic.claude-v2'
-    const command = new InvokeModelCommand({
+    console.log('mode-id: ', model)
+    const command = new ConverseCommand({
         modelId: model,
         contentType: 'application/json',
         accept: 'application/json',
-        inputText: intent
+        messages: [{role: 'user', content: [{text: intent}]}],
+        system: 'You are a helpful assistant that helps software developers fix their CI/CD pipeline issues.',
+        inferenceConfig: {
+            maxTokens: 1000,
+            temperature: 0.7
+        }
     })
 
     console.log('command sent: ', command)
 
     const response = await client.send(command)
     console.log('resp: ', response)
-    const parsed = JSON.parse(new TextDecoder().decode(response.body))
+    // const parsed = JSON.parse(new TextDecoder().decode(response.body))
+    const parsed = response.output?.message?.content?.[0]?.text
     console.log('output: ', parsed)
-
-    return parsed.ouput ?? parsed.generated_text ?? parsed.text ?? parsed
+    return parsed
 }
